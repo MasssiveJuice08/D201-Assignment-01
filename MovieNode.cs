6ecf03f9e44bac28b076c6608921ca0ed16b441f
MassiveJuice <nicholas.h.rowell@gmail.com>
2026-05-14 15:16:18 +1200

    Implement Movie Class + switch to ListView
    - Implemented Movie class to store Movie objects
    - implemented MovieLinkedList collection
    - switched from using DataGrid in main window to ListView for simplicity

---
+++ MovieNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D201_Assignment_01
+{
+  internal class MovieNode
+  {
+    public Movie Data { get; set; }
+    public MovieNode Next { get; set; }
+
+    public MovieNode(Movie movie)
+    {
+      Data = movie;
+      Next = null;
+    }
+  }
+}
