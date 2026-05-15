a3742cdbffca6cc9c743b43d1f1961d54ffcf644
MassiveJuice <nicholas.h.rowell@gmail.com>
2026-05-15 23:25:38 +1200

    implement UserLinkedList for user management
    - based off MovieLinkedList, without hashtable functionality or sort methods

---
+++ UserLinkedList.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D201_Assignment_01
+{
+  internal class UserLinkedList
+  {
+    private UserNode head;
+    private UserNode tail;
+    public int Count { get; private set; }
+
+    public void AddLast(User user)
+    {
+      UserNode newNode = new UserNode(user);
+      if (head == null)
+      {
+        head = newNode;
+        tail = newNode;
+      }
+      else
+      {
+        tail.Next = newNode;
+        tail = newNode;
+      }
+      Count++;
+    }
+
+    public bool Remove(User user)
+    {
+      if (head == null) return false;
+
+      if (head.Data == user)
+      {
+          head = head.Next;
+          if (head == null)
+          {
+              tail = null;
+          }
+          Count--;
+          return true;
+      }
+
+      UserNode current = head;
+      while (current.Next != null)
+      {
+          if (current.Next.Data == user)
+          {
+              current.Next = current.Next.Next;
+              if (current.Next == null)
+              {
+                  tail = current;
+              }
+              Count--;
+              return true;
+          }
+          current = current.Next;
+      }
+      return false;
+    }
+
+    public List<User> ToList()
+    {
+      List<User> users = new List<User>();
+      UserNode current = head;
+      while (current != null)
+      {
+        users.Add(current.Data);
+        current = current.Next;
+      }
+      return users;
+    }
+
+    public void Clear()
+    {
+      head = null;
+      tail = null;
+      Count = 0;
+    }
+  }
+}
