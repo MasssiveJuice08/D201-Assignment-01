7747bd670b94af02718cc9294af5561a9dc2a893
MassiveJuice <nicholas.h.rowell@gmail.com>
2026-05-17 03:03:08 +1200

    Implement unique userID for users
    - added string UserID field to User class. This represents membership IDs the fictional film club may have for its members
    - if adding a user with the same firstName & lastName as an existing user, prompt a yes/no dialogue before adding user (different film club members could have matching first and last names, e.g., "John Smith")
    - to-do: prevent adding a user if userID is a duplicate of an existing userID

--- User.cs
+++ User.cs
@@ -8,18 +8,20 @@ namespace D201_Assignment_01
 {
   internal class User
   {
+    public string UserID { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
 
-    public User(string firstName, string lastName)
+    public User(string userID, string firstName, string lastName)
     {
+      UserID = userID;
       FirstName = firstName;
       LastName = lastName;
     }
 
     public override string ToString()
     {
-      return $"{FirstName} {LastName}";
+      return $"{FirstName} {LastName} (ID: {UserID}";
     }
   }
 }
--- UserLinkedList.cs
+++ UserLinkedList.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace D201_Assignment_01
 {
@@ -14,6 +15,22 @@ namespace D201_Assignment_01
 
     public void AddLast(User user)
     {
+      // check for duplicate names
+      if (HasDuplicateName(user.FirstName, user.LastName))
+      {
+        MessageBoxResult result = MessageBox.Show(
+          $"A user with the name {user.FirstName} {user.LastName} already exists.\n\n" +
+          $"Would you like to add this user anyway?",
+          "Duplicate Name",
+          MessageBoxButton.YesNo,
+          MessageBoxImage.Question);
+
+        if (result == MessageBoxResult.No)
+        {
+          return; // user not added
+        }
+      }
+      
       UserNode newNode = new UserNode(user);
       if (head == null)
       {
@@ -26,6 +43,21 @@ namespace D201_Assignment_01
         tail = newNode;
       }
       Count++;
+    }
+
+    public bool HasDuplicateName(string firstName, string lastName)
+    {
+      UserNode current = head;
+      while (current != null)
+      {
+        if (current.Data.FirstName.Equals(firstName, StringComparison.OrdinalIgnoreCase) &&
+          current.Data.LastName.Equals(lastName, StringComparison.OrdinalIgnoreCase))
+        {
+          return true;
+        }
+        current = current.Next;
+      }
+      return false;
     }
 
     public bool Remove(User user)
